d57d623d49933c40ea3f3fa7cf6b064b1fd2f5b0
Ali <nehrani@rapsodo.com>
2020-01-31 14:18:22 +0300

    Including OD repo v2

---
+++ V2/yolo2/loss.py
@@ -0,0 +1,170 @@
+# -*- coding=utf-8 -*-
+#!/usr/bin/python3
+
+from tensorflow.keras import backend as K
+from yolo2.postprocess import yolo2_head
+
+
+def _smooth_labels(y_true, label_smoothing):
+    label_smoothing = K.constant(label_smoothing, dtype=K.floatx())
+    return y_true * (1.0 - label_smoothing) + 0.5 * label_smoothing
+
+
+def yolo2_loss(args, anchors, num_classes, label_smoothing=0, use_crossentropy_loss=False, use_crossentropy_obj_loss=False, rescore_confidence=False):
+    """YOLOv2 loss function.
+
+    Parameters
+    ----------
+    yolo_output : tensor
+        Final convolutional layer features.
+
+    true_boxes : tensor
+        Ground truth boxes tensor with shape [batch, num_true_boxes, 5]
+        containing box x_center, y_center, width, height, and class.
+
+    y_true : array
+        output of preprocess_true_boxes, with shape [conv_height, conv_width, num_anchors, 6]
+
+    anchors : tensor
+        Anchor boxes for model.
+
+    num_classes : int
+        Number of object classes.
+
+    rescore_confidence : bool, default=False
+        If true then set confidence target to IOU of best predicted box with
+        the closest matching ground truth box.
+
+
+    Returns
+    -------
+    total_loss : float
+        total mean YOLOv2 loss across minibatch
+    """
+    (yolo_output, true_boxes, y_true) = args
+    num_anchors = len(anchors)
+    yolo_output_shape = K.shape(yolo_output)
+    input_shape = yolo_output_shape[1:3] * 32
+    object_scale = 5
+    no_object_scale = 1
+    class_scale = 1
+    coordinates_scale = 1
+    object_mask = y_true[..., 4:5]
+    pred_xy, pred_wh, pred_confidence, pred_class_prob = yolo2_head(
+        yolo_output, anchors, num_classes, input_shape)
+
+    # Unadjusted box predictions for loss.
+    # TODO: Remove extra computation shared with yolo2_head.
+    batch_size = yolo_output_shape[0] # batch size, tensor
+    batch_size_f = K.cast(batch_size, K.dtype(yolo_output))
+
+    feats = K.reshape(yolo_output, [
+        -1, yolo_output_shape[1], yolo_output_shape[2], num_anchors,
+        num_classes + 5
+    ])
+    pred_boxes = K.concatenate(
+        (K.sigmoid(feats[..., 0:2]), feats[..., 2:4]), axis=-1)
+
+    # TODO: Adjust predictions by image width/height for non-square images?
+    # IOUs may be off due to different aspect ratio.
+
+    # Expand pred x,y,w,h to allow comparison with ground truth.
+    # batch, conv_height, conv_width, num_anchors, num_true_boxes, box_params
+    pred_xy = K.expand_dims(pred_xy, 4)
+    pred_wh = K.expand_dims(pred_wh, 4)
+
+    pred_wh_half = pred_wh / 2.
+    pred_mins = pred_xy - pred_wh_half
+    pred_maxes = pred_xy + pred_wh_half
+
+    true_boxes_shape = K.shape(true_boxes)
+
+    # batch, conv_height, conv_width, num_anchors, num_true_boxes, box_params
+    true_boxes = K.reshape(true_boxes, [
+        true_boxes_shape[0], 1, 1, 1, true_boxes_shape[1], true_boxes_shape[2]
+    ])
+    true_xy = true_boxes[..., 0:2]
+    true_wh = true_boxes[..., 2:4]
+
+    # Find IOU of each predicted box with each ground truth box.
+    true_wh_half = true_wh / 2.
+    true_mins = true_xy - true_wh_half
+    true_maxes = true_xy + true_wh_half
+
+    intersect_mins = K.maximum(pred_mins, true_mins)
+    intersect_maxes = K.minimum(pred_maxes, true_maxes)
+    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
+
+    pred_areas = pred_wh[..., 0] * pred_wh[..., 1]
+    true_areas = true_wh[..., 0] * true_wh[..., 1]
+
+    union_areas = pred_areas + true_areas - intersect_areas
+    iou_scores = intersect_areas / union_areas
+
+    # Best IOUs for each location.
+    best_ious = K.max(iou_scores, axis=4)  # Best IOU scores.
+    best_ious = K.expand_dims(best_ious)
+
+    # A detector has found an object if IOU > thresh for some true box.
+    object_detections = K.cast(best_ious > 0.6, K.dtype(best_ious))
+
+    # TODO: Darknet region training includes extra coordinate loss for early
+    # training steps to encourage predictions to match anchor priors.
+
+    # Determine confidence weights from object and no_object weights.
+    # NOTE: YOLOv2 does not use binary cross-entropy. Here we try it.
+    no_object_weights = (no_object_scale * (1 - object_detections) *
+                         (1 - object_mask))
+    if use_crossentropy_obj_loss:
+        no_objects_loss = no_object_weights * K.binary_crossentropy(K.zeros(K.shape(pred_confidence)), pred_confidence, from_logits=False)
+
+        if rescore_confidence:
+            objects_loss = (object_scale * object_mask *
+                            K.binary_crossentropy(best_ious, pred_confidence, from_logits=False))
+        else:
+            objects_loss = (object_scale * object_mask *
+                            K.binary_crossentropy(K.ones(K.shape(pred_confidence)), pred_confidence, from_logits=False))
+    else:
+        no_objects_loss = no_object_weights * K.square(-pred_confidence)
+
+        if rescore_confidence:
+            objects_loss = (object_scale * object_mask *
+                            K.square(best_ious - pred_confidence))
+        else:
+            objects_loss = (object_scale * object_mask *
+                            K.square(1 - pred_confidence))
+    confidence_loss = objects_loss + no_objects_loss
+
+    # Classification loss for matching detections.
+    # NOTE: YOLOv2 does not use categorical cross-entropy loss.
+    #       Here we try it.
+    matching_classes = K.cast(y_true[..., 5], 'int32')
+    matching_classes = K.one_hot(matching_classes, num_classes)
+
+    if label_smoothing:
+        matching_classes = _smooth_labels(matching_classes, label_smoothing)
+
+    if use_crossentropy_loss:
+        classification_loss = (class_scale * object_mask *
+                           K.expand_dims(K.categorical_crossentropy(matching_classes, pred_class_prob, from_logits=False), axis=-1))
+    else:
+        classification_loss = (class_scale * object_mask *
+                           K.square(matching_classes - pred_class_prob))
+
+    # Coordinate loss for matching detection boxes.
+    matching_boxes = y_true[..., 0:4]
+    coordinates_loss = (coordinates_scale * object_mask *
+                        K.square(matching_boxes - pred_boxes))
+
+    confidence_loss_sum = K.sum(confidence_loss) / batch_size_f
+    classification_loss_sum = K.sum(classification_loss) / batch_size_f
+    coordinates_loss_sum = K.sum(coordinates_loss) / batch_size_f
+    total_loss = 0.5 * (
+        confidence_loss_sum + classification_loss_sum + coordinates_loss_sum)
+
+    # Fit for tf 2.0.0 loss shape
+    total_loss = K.expand_dims(total_loss, axis=-1)
+
+    return total_loss, coordinates_loss_sum, confidence_loss_sum, classification_loss_sum
+
---
+++ V2/yolo3/postprocess_np.py
@@ -0,0 +1,309 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+import numpy as np
+import copy
+from scipy.special import expit, softmax
+
+
+def yolo3_head(predictions, anchors, num_classes, input_dims):
+    """
+    YOLO Head to process predictions from YOLO models
+
+    :param num_classes: Total number of classes
+    :param anchors: YOLO style anchor list for bounding box assignment
+    :param input_dims: Input dimensions of the image
+    :param predictions: A list of three tensors with shape (N, 19, 19, 255), (N,38, 38, 255) and (N, 76, 76, 255)
+    :return: A tensor with the shape (N, num_boxes, 85)
+    """
+    assert len(predictions) == len(anchors)//3, 'anchor numbers does not match prediction.'
+
+    if len(predictions) == 3: # assume 3 set of predictions is YOLOv3
+        anchor_mask = [[6,7,8], [3,4,5], [0,1,2]]
+    elif len(predictions) == 2: # 2 set of predictions is YOLOv3-tiny
+        anchor_mask = [[3,4,5], [0,1,2]]
+    else:
+        raise ValueError('Unsupported prediction length: {}'.format(len(predictions)))
+
+    results = []
+    for i, prediction in enumerate(predictions):
+        results.append(_yolo3_head(prediction, num_classes, anchors[anchor_mask[i]], input_dims))
+
+    return np.concatenate(results, axis=1)
+
+
+def _yolo3_head(prediction, num_classes, anchors, input_dims):
+    batch_size = np.shape(prediction)[0]
+    stride = input_dims[0] // np.shape(prediction)[1]
+    grid_size = input_dims[0] // stride
+    num_anchors = len(anchors)
+
+    prediction = np.reshape(prediction,
+                            (batch_size, num_anchors * grid_size * grid_size, num_classes + 5))
+
+    box_xy = expit(prediction[:, :, :2])  # t_x (box x and y coordinates)
+    objectness = expit(prediction[:, :, 4])  # p_o (objectness score)
+    objectness = np.expand_dims(objectness, 2)  # To make the same number of values for axis 0 and 1
+
+    grid = np.arange(grid_size)
+    a, b = np.meshgrid(grid, grid)
+
+    x_offset = np.reshape(a, (-1, 1))
+    y_offset = np.reshape(b, (-1, 1))
+
+    x_y_offset = np.concatenate((x_offset, y_offset), axis=1)
+    x_y_offset = np.tile(x_y_offset, (1, num_anchors))
+    x_y_offset = np.reshape(x_y_offset, (-1, 2))
+    x_y_offset = np.expand_dims(x_y_offset, 0)
+
+    box_xy += x_y_offset
+
+    # Log space transform of the height and width
+    anchors = [(a[0] / stride, a[1] / stride) for a in anchors]
+    anchors = np.tile(anchors, (grid_size * grid_size, 1))
+    anchors = np.expand_dims(anchors, 0)
+
+    box_wh = np.exp(prediction[:, :, 2:4]) * anchors
+
+    # Sigmoid class scores
+    class_scores = expit(prediction[:, :, 5:])
+    #class_scores = softmax(prediction[:, :, 5:], axis=-1)
+
+    # Resize detection map back to the input image size
+    box_xy *= stride
+    box_wh *= stride
+
+    # Convert centoids to top left coordinates
+    box_xy -= box_wh / 2
+
+    return np.concatenate([box_xy, box_wh, objectness, class_scores], axis=2)
+
+
+def yolo3_postprocess_np(yolo_outputs, image_shape, anchors, num_classes, model_image_size, max_boxes=100, confidence=0.1, iou_threshold=0.4):
+    predictions = yolo3_head(yolo_outputs, anchors, num_classes, input_dims=model_image_size)
+
+    boxes, classes, scores = yolo3_handle_predictions(predictions,
+                                                max_boxes=max_boxes,
+                                                confidence=confidence,
+                                                iou_threshold=iou_threshold)
+    boxes = yolo3_adjust_boxes(boxes, image_shape, model_image_size)
+
+    return boxes, classes, scores
+
+
+def yolo3_handle_predictions(predictions, max_boxes=100, confidence=0.1, iou_threshold=0.4):
+    boxes = predictions[:, :, :4]
+    box_confidences = np.expand_dims(predictions[:, :, 4], -1)
+    box_class_probs = predictions[:, :, 5:]
+
+    box_scores = box_confidences * box_class_probs
+    box_classes = np.argmax(box_scores, axis=-1)
+    box_class_scores = np.max(box_scores, axis=-1)
+    pos = np.where(box_class_scores >= confidence)
+
+    boxes = boxes[pos]
+    classes = box_classes[pos]
+    scores = box_class_scores[pos]
+
+    # Boxes, Classes and Scores returned from NMS
+    n_boxes, n_classes, n_scores = nms_boxes(boxes, classes, scores, iou_threshold, confidence=confidence)
+
+    if n_boxes:
+        boxes = np.concatenate(n_boxes)
+        classes = np.concatenate(n_classes)
+        scores = np.concatenate(n_scores)
+        boxes, classes, scores = filter_boxes(boxes, classes, scores, max_boxes)
+
+        return boxes, classes, scores
+
+    else:
+        return [], [], []
+
+
+def filter_boxes(boxes, classes, scores, max_boxes):
+    '''
+    Sort the prediction boxes according to score
+    and only pick top "max_boxes" ones
+    '''
+    # sort result according to scores
+    sorted_indices = np.argsort(scores)
+    sorted_indices = sorted_indices[::-1]
+    nboxes = boxes[sorted_indices]
+    nclasses = classes[sorted_indices]
+    nscores = scores[sorted_indices]
+
+    # only pick max_boxes
+    nboxes = nboxes[:max_boxes]
+    nclasses = nclasses[:max_boxes]
+    nscores = nscores[:max_boxes]
+
+    return nboxes, nclasses, nscores
+
+
+
+def soft_nms_boxes(boxes, classes, scores, iou_threshold, confidence=0.1, is_soft=True, use_exp=False, sigma=0.5):
+    nboxes, nclasses, nscores = [], [], []
+    for c in set(classes):
+        # handle data for one class
+        inds = np.where(classes == c)
+        b = boxes[inds]
+        c = classes[inds]
+        s = scores[inds]
+
+        # make a data copy to avoid breaking
+        # during nms operation
+        b_nms = copy.deepcopy(b)
+        c_nms = copy.deepcopy(c)
+        s_nms = copy.deepcopy(s)
+
+        while len(s_nms) > 0:
+            # pick the max box and store, here
+            # we also use copy to persist result
+            i = np.argmax(s_nms, axis=-1)
+            nboxes.append(copy.deepcopy(b_nms[i]))
+            nclasses.append(copy.deepcopy(c_nms[i]))
+            nscores.append(copy.deepcopy(s_nms[i]))
+
+            # swap the max line and last line
+            b_nms[[i,-1],:] = b_nms[[-1,i],:]
+            c_nms[[i,-1]] = c_nms[[-1,i]]
+            s_nms[[i,-1]] = s_nms[[-1,i]]
+
+            # get box coordinate and area
+            x = b_nms[:, 0]
+            y = b_nms[:, 1]
+            w = b_nms[:, 2]
+            h = b_nms[:, 3]
+
+            areas = w * h
+
+            # check IOU
+            xx1 = np.maximum(x[-1], x[:-1])
+            yy1 = np.maximum(y[-1], y[-1])
+            xx2 = np.minimum(x[-1] + w[-1], x[:-1] + w[:-1])
+            yy2 = np.minimum(y[-1] + h[-1], y[:-1] + h[:-1])
+
+            w1 = np.maximum(0.0, xx2 - xx1 + 1)
+            h1 = np.maximum(0.0, yy2 - yy1 + 1)
+
+            inter = w1 * h1
+            iou = inter / (areas[-1] + areas[:-1] - inter)
+
+            # drop the last line since it has been record
+            b_nms = b_nms[:-1]
+            c_nms = c_nms[:-1]
+            s_nms = s_nms[:-1]
+
+            if is_soft:
+                # Soft-NMS
+                if use_exp:
+                    # score refresh formula:
+                    # score = score * exp(-(iou^2)/sigma)
+                    s_nms = s_nms * np.exp(-(iou * iou) / sigma)
+                else:
+                    # score refresh formula:
+                    # score = score * (1 - iou) if iou > threshold
+                    depress_mask = np.where(iou > iou_threshold)[0]
+                    s_nms[depress_mask] = s_nms[depress_mask]*(1-iou[depress_mask])
+                keep_mask = np.where(s_nms >= confidence)[0]
+            else:
+                # normal Hard-NMS
+                keep_mask = np.where(iou <= iou_threshold)[0]
+
+            # keep needed box for next loop
+            b_nms = b_nms[keep_mask]
+            c_nms = c_nms[keep_mask]
+            s_nms = s_nms[keep_mask]
+
+    # reformat result for output
+    nboxes = [np.array(nboxes)]
+    nclasses = [np.array(nclasses)]
+    nscores = [np.array(nscores)]
+    return nboxes, nclasses, nscores
+
+
+def nms_boxes(boxes, classes, scores, iou_threshold, confidence=0.1):
+    nboxes, nclasses, nscores = [], [], []
+    for c in set(classes):
+        inds = np.where(classes == c)
+        b = boxes[inds]
+        c = classes[inds]
+        s = scores[inds]
+
+        x = b[:, 0]
+        y = b[:, 1]
+        w = b[:, 2]
+        h = b[:, 3]
+
+        areas = w * h
+        order = s.argsort()[::-1]
+
+        keep = []
+        while order.size > 0:
+            i = order[0]
+            keep.append(i)
+
+            xx1 = np.maximum(x[i], x[order[1:]])
+            yy1 = np.maximum(y[i], y[order[1:]])
+            xx2 = np.minimum(x[i] + w[i], x[order[1:]] + w[order[1:]])
+            yy2 = np.minimum(y[i] + h[i], y[order[1:]] + h[order[1:]])
+
+            w1 = np.maximum(0.0, xx2 - xx1 + 1)
+            h1 = np.maximum(0.0, yy2 - yy1 + 1)
+
+            inter = w1 * h1
+            ovr = inter / (areas[i] + areas[order[1:]] - inter)
+            inds = np.where(ovr <= iou_threshold)[0]
+            order = order[inds + 1]
+
+        keep = np.array(keep)
+
+        nboxes.append(b[keep])
+        nclasses.append(c[keep])
+        nscores.append(s[keep])
+    return nboxes, nclasses, nscores
+
+
+def yolo3_adjust_boxes(boxes, img_shape, model_image_size):
+    if boxes is None or len(boxes) == 0:
+        return []
+
+    image_shape = np.array(img_shape, dtype='float32')
+    #height, width = image_shape
+    width, height = image_shape
+    adjusted_boxes = []
+
+    model_image_size = np.array(model_image_size, dtype='float32')
+
+    new_shape = np.round(image_shape * np.min(model_image_size/image_shape))
+    offset = (model_image_size-new_shape)/2.
+    # here scale should be a unique value
+    # for both width and height
+    scale = np.min(image_shape/new_shape)
+
+    #ratio_x = width / model_image_size[1]
+    #ratio_y = height / model_image_size[0]
+
+    for box in boxes:
+        x, y, w, h = box
+
+        w *= scale
+        h *= scale
+        xmin = (x - offset[0]) * scale
+        ymin = (y - offset[1]) * scale
+        xmax = (x - offset[0]) * scale + w
+        ymax = (y - offset[1]) * scale + h
+
+        # Rescale box coordinates
+        #xmin = int(x * ratio_x)
+        #ymin = int(y * ratio_y)
+        #xmax = int((x + w) * ratio_x)
+        #ymax = int((y + h) * ratio_y)
+
+        ymin = max(0, np.floor(ymin + 0.5).astype('int32'))
+        xmin = max(0, np.floor(xmin + 0.5).astype('int32'))
+        ymax = min(height, np.floor(ymax + 0.5).astype('int32'))
+        xmax = min(width, np.floor(xmax + 0.5).astype('int32'))
+        adjusted_boxes.append([xmin,ymin,xmax,ymax])
+
+    return np.array(adjusted_boxes,dtype=np.int32)
+
